880851b95148c3d4746d229f3fe869c82d1b82e8
Clyde <kipkoechclyde546@gmail.com>
2024-10-11 16:48:13 +0300

    initial commit

---
+++ src/components/Transaction.js
@@ -0,0 +1,15 @@
+import React, {useContext} from 'react';
+import  { GlobalContext}  from '../context/Global';
+
+export default function Transaction({transaction}) {
+    const sign = transaction.amount < 0 ? '-' : '+';
+    const {deleteTransaction} = useContext(GlobalContext)
+  return (
+    <div>
+      <li className={transaction.Amount < 0 ? 'minus': 'plus'}>
+        {transaction.text}
+           <span>{sign}${Math.abs(transaction.Amount)}</span><button onClick ={() => deleteTransaction(transaction.id)} className="delete-btn">x</button>
+        </li> 
+    </div>
+  )
+}
---
+++ src/components/mortage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const mortage = () => {
+  return (
+    <div>
+     
+    </div>
+  )
+}
+
+export default mortage
